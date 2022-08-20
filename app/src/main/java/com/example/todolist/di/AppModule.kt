73aead70b1fde459f4732eaa358d858fca500147
Roy Devpreyo <devedroy.dr@gmail.com>
2022-08-20 15:34:55 +0530

    Todo List V1

---
+++ app/src/main/java/com/example/todolist/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.todolist.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.todolist.data.TodoDatabase
+import com.example.todolist.data.TodoRepository
+import com.example.todolist.data.TodoRepositoryImplementation
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImplementation(db.dao)
+    }
+}
